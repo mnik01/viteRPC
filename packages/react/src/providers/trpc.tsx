ea7d1106df7b73a19602dda09a76a3182908c13a
mnik01 <maksimgrinberg01@gmail.com>
2023-03-13 00:16:24 +0500

    fix: cors error

--- packages/react/src/providers/trpc.tsx
+++ packages/react/src/providers/trpc.tsx
@@ -6,13 +6,6 @@ const trpcClient = trpc.createClient({
   links: [
     httpBatchLink({
       url: `${import.meta.env.VITE_TRPC_SERVER_URL}`,
-      // Needed to support session cookies
-      fetch(url, options) {
-        return fetch(url, {
-          ...options,
-          credentials: "include",
-        });
-      },
     }),
   ],
 });
